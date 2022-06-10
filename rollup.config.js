01621dca3d5755352196118e4f2ad78ca65436bb
schbz <schbz@outlook.com>
2022-06-10 13:56:34 -0400

    testing new npm publish settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
-import typescript from 'rollup-plugin-typescript2';
-import vue from 'rollup-plugin-vue';
-import clear from 'rollup-plugin-clear';
-
+import typescript from "rollup-plugin-typescript2";
+import vue from "rollup-plugin-vue";
+import clear from "rollup-plugin-clear";
 
 export default async function config(args) {
   return {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: {
-      dir: 'dist',
-      format: 'cjs',
+      dir: "dist",
+      format: "cjs",
       sourcemap: true,
     },
     plugins: [
@@ -22,9 +21,8 @@ export default async function config(args) {
         },
       }),
       clear({
-        targets: ['./dist'],
-      })
-
+        targets: ["./dist"],
+      }),
     ],
   };
 }
